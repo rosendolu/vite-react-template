d5af82a33b984c1748b4afa5268aa625e08b8d05
rosendo <42633099+rosendolu@users.noreply.github.com>
2023-10-31 14:23:00 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -16,37 +16,37 @@ import './index.css';
 dayjs.locale('zh-cn');
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      refetchOnWindowFocus: false,
-      cacheTime: Infinity,
-      retry: false,
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            refetchOnWindowFocus: false,
+            cacheTime: Infinity,
+            retry: false,
+        },
     },
-  },
 });
 const persister = createSyncStoragePersister({ storage: window.localStorage });
 // const persister = createSyncStoragePersister({ storage: window.localStorage });
 
 persistQueryClientSubscribe({
-  // @ts-ignore
-  queryClient,
-  persister: persister,
-  maxAge: 1000 * 60 * 60 * 24 * 24, // 24 hours
-  // dehydrateOptions: {
-  //   dehydrateMutations: true,
-  //   dehydrateQueries: true,
-  // },
+    // @ts-ignore
+    queryClient,
+    persister: persister,
+    maxAge: 1000 * 60 * 60 * 24 * 24, // 24 hours
+    // dehydrateOptions: {
+    //   dehydrateMutations: true,
+    //   dehydrateQueries: true,
+    // },
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <Toaster position="top-center" reverseOrder={false} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ConfigProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ConfigProvider locale={zhCN}>
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <Toaster position="top-center" reverseOrder={false} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </ConfigProvider>
+    </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  envDir: 'env',
-  envPrefix: ['__APP__'],
+    plugins: [react()],
+    envDir: 'env',
+    envPrefix: ['__APP__'],
 });
